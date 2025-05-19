3313dbce163254c12c38b507c7253c0a71eaed6a
iKREISHI <vadas25@yandex.ru>
2025-05-19 23:14:01 +0500

    added model Locations

--- backend/src/apps/locations/admin.py
+++ backend/src/apps/locations/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Location
 
-# Register your models here.
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    """
+    Администрирование модели Location.
+    """
+    list_display = ('name', 'description')  # Поля для отображения в списке
+    search_fields = ('name',)               # Поле для поиска
+    list_filter = ()                        # Фильтры (если нужны)
+    ordering = ('name',)                    # Сортировка по умолчанию
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description'),
+        }),
+    )
---
+++ backend/src/apps/locations/models/locations.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Location(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Название местоположения',
+    )
+    description = models.TextField(
+        verbose_name='Описание местоположения',
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Местоположение инвентарного оборудования'
+        verbose_name_plural = 'Местоположения инвентарного оборудования'
