0a55c33b6b15c11e3b8dbd3b52b7045bff066e4a
iKREISHI <vadas25@yandex.ru>
2025-05-19 23:05:14 +0500

    init models equipments and reservations

--- backend/src/apps/users/admin.py
+++ backend/src/apps/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from apps.users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUser(UserAdmin):
+    list_display = ('id', 'username', 'email', 'is_active',)
+    fieldsets = (
+        (None, {'fields': (
+            'last_name', 'first_name', 'patronymic',
+            'username', 'password',
+            'email', 'telegram_chat_id'
+        )}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    readonly_fields = ('id', 'last_login', 'date_joined')
