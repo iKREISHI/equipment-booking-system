d0c236b2a306e990065d25cca3c061ce0813200a
iKREISHI <vadas25@yandex.ru>
2025-06-01 16:42:16 +0500

    added CORS

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django_filters',
     'drf_spectacular',
     'drf_spectacular_sidecar',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +62,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+    'http://localhost:8000',
+    'http://localhost:3000',
+    'http://localhost:5174',
 ]
 
 ROOT_URLCONF = 'config.urls'
