9df16a285785906a32e870bd784c2114908fd785
iKREISHI <vadas25@yandex.ru>
2025-06-03 17:35:14 +0500

    added inventory status entrypoints

--- backend/src/utils/management/commands/create_inventory_status.py
+++ backend/src/utils/management/commands/create_inventory_status.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from apps.locations.models import Location
+from apps.equipments.models.inventory_equipment_status import InventoryEquipmentStatus
 from utils.entrypoints.data.inventory_status import inventory_equipment_status
 
 
@@ -9,14 +9,9 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        """
-        Проходим по списку словарей locations и для каждой записи:
-        - Если локация с таким именем уже существует, обновляем её описание.
-        - Если не существует, создаём новую локацию.
-        """
         for status in inventory_equipment_status:
             name = status["name"]
-            Location.objects.update_or_create(
+            InventoryEquipmentStatus.objects.update_or_create(
                 name=name,
             )
         self.stdout.write(self.style.SUCCESS("Статусы инвентарного оборудования успешно созданы или обновлены."))
