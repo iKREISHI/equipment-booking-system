6ea8c20a5194dcac878f73aa09de1bedd4c42d6d
shiro-kreishi <vadas25@yandex.ru>
2025-06-21 18:17:36 +0500

    added new home page

--- backend/src/web/views/home_page.py
+++ backend/src/web/views/home_page.py
@@ -1,9 +1,21 @@
+from django.core.paginator import Paginator
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import View
+from django.views import View
 
+from apps.reservations.models import Reservation   # имя модели оставьте своё
 
-class HomePage(View):
-    _template_name = 'pages/homepage.html'
+class HomePage(LoginRequiredMixin, View):
+    template_name = "pages/homepage.html"
+    paginate_by   = 10                # элементов на страницу
 
     def get(self, request):
-        return render(request, self._template_name)
+        qs = (Reservation.objects
+            .select_related("equipment")
+            .filter(status=2)
+            .order_by("start_time"))
+
+        paginator  = Paginator(qs, self.paginate_by)
+        page_obj   = paginator.get_page(request.GET.get("page"))
+
+        return render(request, self.template_name, {"page_obj": page_obj})
